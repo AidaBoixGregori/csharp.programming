bd9d2e2815a01507fbee4b7c8bc1d4fe78ab50ee
Aida Boix Gregori <aidaboixgregori@gmail.com>
2023-07-11 21:30:31 +0200

    003 - Times Table Of 15

---
+++ 01-firstStepsWithCSharp/003-TimesTableOf15.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class TimesTableOf15
+{
+    public static void Main()
+    {
+        Console.WriteLine("15 x 0 = 0");
+        Console.WriteLine("15 x 1 = 15");
+        Console.WriteLine("15 x 2 = 30");
+        Console.WriteLine("15 x 3 = 45");
+        Console.WriteLine("15 x 4 = 60");
+        Console.WriteLine("15 x 5 = 75");
+        Console.WriteLine("15 x 6 = 90");
+        Console.WriteLine("15 x 7 = 105");
+        Console.WriteLine("15 x 8 = 120");
+        Console.WriteLine("15 x 9 = 135");
+        Console.WriteLine("15 x 10 = 150");
+    }
+}
